7007a92d2709c5e0dd0360ed07189888ee7bac88
lepakshiagarwal <60332064+lepakshiagarwal@users.noreply.github.com>
2020-05-19 15:33:59 -0400

    Hardcoded game

---
+++ Program.py
@@ -0,0 +1,9 @@
+user = input()
+if user == "scissors":
+    print(" Sorry, but computer chose rock")
+elif user == "rock":
+    print("Sorry, but computer chose paper")
+elif user == "paper":
+    print("Sorry, but computer chose scissors")
+else:
+    print("Enter valid option")
